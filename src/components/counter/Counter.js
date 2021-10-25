e2080db6292c647f4f54f138716f611f3f190698
이도현 <ksmfou98@naver.com>
2021-10-25 21:22:15 +0900

    Feat: plus, minus button UI

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -6,8 +6,11 @@ function Counter() {
   return (
     <div>
       <h3 data-testid="counter">{counter}</h3>
+      <div>
+        <button data-testid="minus-button">-</button>
+        <button data-testid="plus-button">+</button>
+      </div>
     </div>
   );
 }
 export default Counter;
- 
