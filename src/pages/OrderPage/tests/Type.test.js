5b9caebe6e44dd2711c8319d69bc86349de87452
이도현 <ksmfou98@naver.com>
2021-11-06 21:41:39 +0900

    Test: when fetching product datas, face an error

--- src/pages/OrderPage/tests/Type.test.js
+++ src/pages/OrderPage/tests/Type.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from "@testing-library/react";
+import { rest } from "msw";
+import { server } from "../../../mocks/server";
 import Type from "../Type";
 
 test("display product images from server", async () => {
@@ -12,4 +14,17 @@ test("display product images from server", async () => {
 
   const allText = productImages.map((element) => element.alt);
   expect(allText).toEqual(["America product", "England product"]);
+});
+
+test("when fetching product datas, face an error", async () => {
+  server.resetHandlers(
+    rest.get("http://localhost:5000/products", (req, res, ctx) => {
+      res(ctx.status(500));
+    })
+  );
+
+  render(<Type orderType="products" />);
+
+  const errorBanner = await screen.findByTestId("error-banner");
+  expect(errorBanner).toHaveTextContent("에러가 발생했습니다.");
 });
